a95eb0af4797561ca4e681be1f402c7bfe0f6102
quliyevresul7777 <quliyev.resul7777@gmail.com>
2026-05-12 16:38:46 +0400

    Create Position.cs

---
+++ Models/Position.cs
@@ -0,0 +1,8 @@
+namespace SalaryManagementSystem.Models;
+
+public class Position
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal BaseSalary { get; set; }
+}
